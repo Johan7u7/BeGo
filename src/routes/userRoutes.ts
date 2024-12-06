c2188fb9302e0fadc922f954c8a070f9ad69e100
Johan7u7 <johan123ali@gmail.com>
2024-12-06 14:05:52 -0600

    BaseClean

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,10 +7,4 @@ const router = express.Router();
 router.post('/register', registerUser); // Ruta para registrar un usuario
 router.post('/login', loginUser); // Ruta para iniciar sesión
 
-// Ruta protegida: solo accesible con token válido
-router.get('/profile', authMiddleware, (req, res) => {
-  // Aquí accedes a req.userId gracias al middleware de autenticación
-  res.status(200).json({ message: 'User profile', userId: req.userId });
-});
-
 export default router;
